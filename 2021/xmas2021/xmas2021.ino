fa5a6263fca51dd5c8e2dd0c796f23d5d0101b4c
Ian Tabor <40205011+mintynet@users.noreply.github.com>
2021-11-22 10:38:49 +0000

    Update xmas2021.ino

--- 2021/xmas2021/xmas2021.ino
+++ 2021/xmas2021/xmas2021.ino
@@ -1,14 +1,37 @@
+#define POWER 1
+
+#if POWER
+#include <avr/sleep.h>
+boolean Power __attribute__ ((section (".noinit")));
+
+void PowerDown () {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  ADCSRA &= ~(1<<ADEN);     // Turn off ADC to save power
+  sleep_enable();
+  sleep_cpu();
+}
+#endif
+
 int delayCount = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+#if POWER
+  Power = !Power;
+  if (Power) {
+    pinMode(0,OUTPUT);
+    pinMode(1,OUTPUT);
+    pinMode(2,OUTPUT);
+  } else {
+    PowerDown();
+  }
+#else
   pinMode(0,OUTPUT);
   pinMode(1,OUTPUT);
   pinMode(2,OUTPUT);
+#endif  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite (2,HIGH);
   delay(delayCount/20);
   digitalWrite (1,HIGH);
